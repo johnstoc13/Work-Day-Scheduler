f123f952588acb87af4b4a28361565a65c82032b
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-07-25 17:03:33 -0400

    added event listeners to save btns

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,26 @@
-$(function(){
+$(document).ready(function(){
     
     // Loop through 9 hours schedule and create elements
     // Cited:  Worked with tutor here
     for (var i = 9; i < 18; i++) {
         let $timeColumn;
         if (i < 12) {
-            $timeColumn = $("<div class='col-2 time hour'>").text(i + "AM").data("hour", i);    
+            $timeColumn = $("<div class='col-1 time hour text-right'>").text(i + "AM").data("hour", i);    
         } else if (i == 12) {
-            $timeColumn = $("<div class='col-2 time hour'>").text(i + "PM").data("hour", i);    
+            $timeColumn = $("<div class='col-1 time hour text-right'>").text(i + "PM").data("hour", i);    
         } else {
-            $timeColumn = $("<div class='col-2 time hour'>").text((i - 12) + "PM").data("hour", i);    
+            $timeColumn = $("<div class='col-1 time hour text-right'>").text((i - 12) + "PM").data("hour", i);    
         }
         // Define variables
         const $scheduleRow = $("<div class='row'>");
-        const $eventColumn = $("<input type='text' class='col-8 event'>");
-        const $saveColumn = $("<button class='saveBtn fas fa-save col-2 save'>");
+        const $eventColumn = $("<input type='text' class='col-10 text-body event'>");
+        const $saveColumn = $("<button class='saveBtn fas fa-save fa-2x col-1 save'>");
         $scheduleRow.append($timeColumn, $eventColumn, $saveColumn);
         $(".container").append($scheduleRow);
     }
 
-    
     // Put current day, month, and date onto page
     $("#currentDay").append(moment().format("dddd, MMMM Do"));
-
-    moment().hour();
-    // console.log(moment().hour());
 
     // Set background colors in event columns
     // Cited:  Worked with tutor here
@@ -41,7 +37,30 @@ $(function(){
         })
     }
 
+    // Set event listeners to save buttons
+    $(".saveBtn").on("click", function(event) {
+        let taskArray = {};
+        $(".text-body").each(function(currentIndex, currentEl) {
+            taskArray[currentIndex] = $(currentEl).val().trim();
+        });
+        console.log(taskArray);
+    })
+
+    // Load tasks from local storage
+    function loadTasks() {
+        const storedTasks = JSON.parse(localStorage.getItem("taskArray"));
+        if (storedTasks) {
+            taskArray = storedTasks;
+        }
+    };
+
+
+
+
+
+
     // Call function when page opens or refreshes
     setTimeColor();
+    loadTasks();
 
 })
